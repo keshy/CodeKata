248a58629d3294644f78c79e9398c81fdfc4c2a1
Krishnan <keshi8086@gmail.com>
2024-04-18 23:43:23 -0700

    Solved 3sum properly and accepted

--- src/code/kata/lc_top_150/two_ptr/3sum.py
+++ src/code/kata/lc_top_150/two_ptr/3sum.py
@@ -6,7 +6,7 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        result = set()
         list.sort(nums)
         ref = {}
         for i, n in enumerate(nums):
@@ -14,28 +14,31 @@ class Solution:
                 ref[n].append(i)
             else:
                 ref[n] = [i]
-        p1 = 0
-        p2 = len(nums) - 1
-        while p1 < p2:
-            # add p1 and p2
-            t_sum = nums[p1] + nums[p2]
-            # if sum is < 0 and dict look up is not returning anything - then move p1.
-            mid_idxs = ref.get(-t_sum, -1)
-            if mid_idxs != -1:
-                for id in mid_idxs:
-                    if id != p1 and id != p2:
-                        s = sorted([nums[p1], nums[id], nums[p2]])
-                        if s not in result:
-                            result.append(s)
-            if t_sum < 0:
-                p1 += 1
-            else:
-                p2 -= 1
-        return result
+
+        for i in range(0, len(nums)):
+            p1 = i
+            p2 = len(nums) - 1
+            if nums[i] == nums[i - 1] and i > 0:
+                continue
+            while p1 < p2:
+                # add p1 and p2
+                t_sum = nums[p1] + nums[p2]
+                # if sum is < 0 and dict look up is not returning anything - then move p1.
+                mid_idxs = ref.get(-t_sum, -1)
+                if mid_idxs != -1:
+                    for id in mid_idxs:
+                        if id > p1 and id < p2:
+                            s = (nums[p1], nums[id], nums[p2])
+                            result.add(s)
+                if t_sum < nums[i]:
+                    p1 += 1
+                else:
+                    p2 -= 1
+        return [[x[0], x[1], x[2]] for x in result]
 
 
 if __name__ == '__main__':
     c = Solution()
     print(c.threeSum([-1, 0, 1, 2, -1, -4]))
-    print(c.threeSum([0,0,0]))
-    print(c.threeSum([0,1,1]))
+    print(c.threeSum([0, 0, 0]))
+    print(c.threeSum([0, 1, 1]))
